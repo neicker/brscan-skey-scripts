42648582f95a9a18043d953f617f8b74f9136f1c
Norbert Eicker <norbert.eicker@gmx.de>
2017-04-16 15:33:07 +0200

    Automatically generate PDF files from scan

--- scantofile-0.2.4-0.sh
+++ scantofile-0.2.4-0.sh
@@ -8,7 +8,7 @@ set +o noclobber
 #   
 #       100,200,300,400,600
 #
-resolution=100
+resolution=300
 device=$1
 mkdir -p ~/brscan
 if [ "`which usleep  2>/dev/null `" != '' ];then
@@ -16,7 +16,28 @@ if [ "`which usleep  2>/dev/null `" != '' ];then
 else
     sleep  0.01
 fi
-output_file=~/brscan/brscan_"`date +%Y-%m-%d-%H-%M-%S`"".pnm"
-#echo "scan from $2($device) to $output_file"
-scanimage --device-name "$device" --resolution $resolution> $output_file  2>/dev/null
+
+# Close a bunch of file descriptors
+#for i in {4..11}; do
+#	exec ${i}>&-
+#done
+exec 4>&-
+exec 5>&-
+exec 6>&-
+exec 7>&-
+exec 8>&-
+exec 9>&-
+exec 10>&-
+exec 11>&-
+
+output_file=~/brscan/brscan_"`date +%Y-%m-%d-%H-%M-%S`"".pdf"
+
+echo "scan from $2($device) to $output_file"
+
+scanimage --device-name "$device" --resolution $resolution 	\
+		-x 210 -y 297  2>/dev/null | \
+	pnmtops -dpi=${resolution} -equalpixels 2>/dev/null |	\
+	gs -q -sDEVICE=pdfwrite -r${resolution} -sPAPERSIZE=a4	\
+		-dBATCH -sOutputFile=${output_file} - 2>/dev/null
+
 echo  $output_file is created.
